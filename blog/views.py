4f86981bedcf60fb569a32d26c89ef6805f89cf3
yumemito <yumemi_now_dreaming@yahoo.co.jp>
2017-05-07 21:11:06 +0200

    view edited

--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,8 @@ from .models import Post
 
 def post_list(request):
     post = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
-    return render(request, "blog/post_list.html"{})
-    
+    return render(request, "blog/post_list.html" {})
+
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
