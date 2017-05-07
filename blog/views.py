143421f0585417c790dcecce34b319e2d1f5373b
yumemito <yumemi_now_dreaming@yahoo.co.jp>
2017-05-07 16:29:14 +0200

    added viewe

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 def post_list(request):
     return render(request, "blog/post_list.html",{})
-    
-# Create your views here.
+from django.shortcuts import render
+from .models import Post
+Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+def post_list(request):
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    return render(request, "blog/post_list.html"{})
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request)
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    return render(request, "blog/post_list.html", {"posts":posts})
+    # Create your views here.
